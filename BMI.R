06557cbf440747ce5f3e87bc2de7caa4e306c3cc
LS <LS31@users.noreply.github.com>
2017-04-20 14:34:40 +0200

    Code style

--- BMI.R
+++ BMI.R
@@ -2,8 +2,7 @@
 #' @param weight Weight in kg.
 #' @param length Length in cm.
 #' @return BMI in kg/m^2.
-calculateBMI = function(weight, length) {
-  length = length / 100
-  bmi = weight / (length^2)
-  return(bmi)
+calculate_bmi <- function(weight, length) {
+  length <- length / 100
+  weight / (length ^ 2)
 }
