bcde2cf2ca5dc79f5836692f33ef0041169bc8cb
LS <LS31@users.noreply.github.com>
2017-04-20 14:35:30 +0200

    Code style

--- BSA.R
+++ BSA.R
@@ -2,7 +2,6 @@
 #' @param weight Weight in kg.
 #' @param length Length in cm.
 #' @return BSA in m^2.
-calculateBSA = function(weight, length) {
-  bsa = ((weight * length) / 3600) ^ (1 / 2)
-  return(bsa)
+calculate_bsa <- function(weight, length) {
+  (weight * length / 3600) ^ (1 / 2)
 }
