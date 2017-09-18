51907af1b0440246fdd13e8251b5cb4490d3f290
LS <LS31@users.noreply.github.com>
2017-09-18 14:11:46 +0200

    Delete BSA.R

--- BSA.R
+++
@@ -1,7 +0,0 @@
-#' Calculates BSA (body surface area) (Mosteller, 1987).
-#' @param weight Weight in kg.
-#' @param length Length in cm.
-#' @return BSA in m^2.
-calculate_bsa <- function(weight, length) {
-  (weight * length / 3600) ^ (1 / 2)
-}
