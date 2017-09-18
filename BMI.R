2f3a7cdea0387575c0c0a7969072edba904f97ee
LS <LS31@users.noreply.github.com>
2017-09-18 14:11:41 +0200

    Delete BMI.R

--- BMI.R
+++
@@ -1,8 +0,0 @@
-#' Calculates BMI.
-#' @param weight Weight in kg.
-#' @param length Length in cm.
-#' @return BMI in kg/m^2.
-calculate_bmi <- function(weight, length) {
-  length <- length / 100
-  weight / (length ^ 2)
-}
