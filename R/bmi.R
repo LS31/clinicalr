cabbf51e934bc5557205fcd1a31c43cc20871bbf
LS31 <LS31@users.noreply.github.com>
2017-09-19 21:45:00 +0200

    Version bump 0.1.1

--- R/bmi.R
+++ R/bmi.R
@@ -3,6 +3,7 @@
 #' @param weight Weight in kg.
 #' @param length Length in cm.
 #' @return BMI in kg/m^2.
+#' @export
 calculate_bmi <- function(weight, length) {
   length <- length / 100
   weight / (length ^ 2)
