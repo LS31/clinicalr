1891870ce4200d79e96b1e8f708249f4afdffe8e
LS31 <LS31@users.noreply.github.com>
2019-12-31 17:09:30 +0100

    Calcium documentation and function name improved.

--- R/electrolytes.R
+++ R/electrolytes.R
@@ -1,22 +1,19 @@
-#' Correct calcium for albumin level
+#' Adjust total calcium for albumin
 #'
-#' Most of the protein-bound calcium is bound to albumin. Lower albumin will lower total calcium levels, but the ionised calcium level will not be lowered that much. Therefore, total calcium levels should be corrected for albumin, since in a state of
-#' hypoalbuminemia the (ionised, relevant) calcium level is underestimated. An alternative approach
-#' is measuring ionised calcium levels directly.
-#'
-#' The formula used here is (calcium + 0.025 * (normal albumin - measured albumin)).
+#' Most of the protein-bound calcium is bound to albumin. In hypoalbuminaemia and hypocalcaemia, the (clinically relevant ionised) calcium level will be underestimated. Therefore, total calcium levels may be adjusted/corrected for albumin. An alternative approach
+#' is measuring ionised calcium levels directly. Calcium is adjusted using the formula: \eqn{\text{calcium}_\text{measured} + 0.025 (\text{albumin}_\text{normal} - \text{albumin}_\text{measured}))}.
 #'
 #' @references Calcium (Bushinsky; 1998): \url{https://doi.org/10.1016/S0140-6736(97)12331-5}.
 #'
-#' @section Caveats: Corrections for albumin are quite poor substitutes for measuring ionised calcium.
+#' @section Caveats: Adjustments for albumin are quite poor substitutes for measuring ionised calcium.
 #'
-#' @param calcium Measured (uncorrected) total calcium level (mmol/l).
+#' @param calcium Measured total calcium level (mmol/l).
 #' @param albumin Measured albumin level (g/l).
 #' @param normal_albumin Normal value of albumin (g/l) (40 by default).
-#' @return Albumin-corrected calcium level (mmol/l).
+#' @return Albumin-adjusted calcium level (mmol/l).
 #' @export
 #' @seealso \code{\link[units]{set_units}}, \code{\link[units]{drop_units}}
-correct_calcium_for_albumin <- function(calcium, albumin, normal_albumin = 40) {
+adjust_calcium_for_albumin <- function(calcium, albumin, normal_albumin = 40) {
   assertthat::assert_that(assertthat::is.number(calcium) | is.na(calcium))
   assertthat::assert_that(assertthat::is.number(albumin) | is.na(albumin))
 
